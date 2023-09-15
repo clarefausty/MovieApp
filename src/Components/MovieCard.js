fb3203551d3a8723096ea71a25b67ce30f52230a
clarefausty <clarefausty@gmail.com>
2023-09-15 15:50:26 +0100

    WIP

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -13,8 +13,8 @@ const MovieCard = ({ poster_path, title, release_date, id }) => {
     <div data-testid="movie-card" className='movie-card-container'>
       <img data-testid="movie-poster" className='movie-img' src={getPosterURL(poster_path)} alt={title} />
       <div className='detail-text' >
-        <h3>{title}</h3>
-        <p>{release_date}</p>
+        <h3 data-testid="movie-title">{title}</h3>
+        <p data-testid="movie-release-date">{release_date}</p>
         <Link id='link' to={`movies/${id}`}><button>View Details</button></Link>
       </div>
     </div>
